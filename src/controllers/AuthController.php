4a5f39cfc0a71fcce577dca902ac4468face7287
Luka <blagicluka666@gmail.com>
2024-03-28 00:35:48 +0100

    Move router, refactor auth

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace Controller;
+
+use PDO;
+use Utility\RequestHandler;
 
 class AuthController
 {
@@ -11,104 +15,24 @@ class AuthController
         $this->conn = $conn;
     }
 
-    public function processRequest(string $method, ?string $id, $user)
+    public function getCollection()
     {
-        switch ($method) {
-            case "POST":
-                switch ($id) {
-                    case "login":
-                        $this->login();
-                        break;
-                    case "logout":
-                        $this->logout();
-                        break;
-                    case "register":
-                        $this->register();
-                        break;
-
-                    default:
-                        http_response_code(400);
-                        echo json_encode(["error" => "Invalid action parameter"]);
-                }
-
-                break;
-            case "GET":
-
-                switch ($id) {
-                    case "user":
-
-                        break;
-                    default:
-                        http_response_code(405);
-                        echo json_encode(["error" => "Invalid action parameter"]);
-                }
-                break;
-            default:
-                http_response_code(405);
-                header("Allow: POST,GET");
-
-        }
     }
-
-    public function login()
+    public function getResource($id)
     {
 
-        $data = json_decode(file_get_contents("php://input"), true);
-        $email = $data["email"];
-        $password = $data["password"];
-        if ($this->auth->login($email, $password)) {
-            $token = $this->auth->generateToken($email);
-            http_response_code(200);
-            echo json_encode([
-                "message" => "Authentication successful",
-                "token" => $token
-            ]);
-        } else {
-            http_response_code(401);
-            echo json_encode([
-                "message" => "Authentication failed"
-            ]);
-        }
     }
-
-    public function logout()
+    public function postCollection()
     {
-        $data = json_decode(file_get_contents("php://input"), true);
-        $token = $data["token"];
-        if ($this->auth->logout($token)) {
-            http_response_code(200);
-            echo json_encode([
-                "message" => "Logout successful"
-            ]);
-        } else {
-            http_response_code(401);
-            echo json_encode([
-                "message" => "Logout failed"
-            ]);
-        }
+
     }
-
-    public function register()
+    public function putResource($id)
     {
-        $data = json_decode(file_get_contents("php://input"), true);
 
-        $name = $data["name"];
-        $surname = $data["surname"];
-        $email = $data["email"];
-        $password = $data["password"];
-        //$profile_picture = $data["profile_picture"];
+    }
+    public function deleteResource($id)
+    {
 
-        if ($this->auth->register($name, $surname, $email, $password)) {
-            http_response_code(200);
-            echo json_encode([
-                "message" => "Registration successful"
-            ]);
-        } else {
-            http_response_code(400);
-            echo json_encode([
-                "message" => "Registration failed or user already exists!"
-            ]);
-        }
     }
 
 
