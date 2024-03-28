fff2893185d6fa23f860a86969ed087abdd9c1e8
Luka <blagicluka666@gmail.com>
2024-03-28 08:40:48 +0100

    Refactor user Model

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -4,19 +4,24 @@ namespace Controller;
 
 use PDO;
 use Utility\RequestHandler;
+use User;
 
 class AuthController
 {
     private $conn;
+    private $user;
+
 
 
     public function __construct(PDO $conn)
     {
         $this->conn = $conn;
+        $this->uesr = new User($conn);
     }
 
     public function getCollection()
     {
+
     }
     public function getResource($id)
     {
