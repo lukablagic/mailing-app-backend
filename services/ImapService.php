967ed6360a44c53c9adb370f3e93ed76e671a1c8
Luka Blagic <blagicluka666@gmail.com>
2024-04-04 22:41:38 +0200

    Implement headers parsing

--- services/ImapService.php
+++ services/ImapService.php
@@ -13,40 +13,64 @@ class ImapService
     private  $ssl = 'ssl';
     private  $folder = 'INBOX';
 
-    public function __construct($server, $port = 993, $folder = 'INBOX', $protocol = 'imap', $ssl = 'ssl',)
+    public function __construct($server, $port = 993, $folder = 'INBOX', $protocol = 'imap', $useSSL = true)
     {
         $this->server = $server;
         $this->port = $port;
         $this->folder = $folder;
         $this->protocol = $protocol;
-        $this->ssl = $ssl;
+        $this->ssl = $useSSL ? 'ssl' : 'novalidate-cert';
     }
 
     public function connect($email, $password): Connection|false
     {
-        $imap = imap_open('{imap.gmail.com:993/imap/ssl}INBOX', $email, $password);
+        $mailbox = "{" . $this->server . ":" . $this->port . "/" . $this->protocol . "/" . $this->ssl . "}" . $this->folder;
+        $imap = imap_open($mailbox, $email, $password);
+
         if ($imap === false) {
-            http_response_code(400);
-            json_encode("Invalid email or password!");
+            return false;
         }
+
         return $imap;
     }
     public function openFolder($imap, $folder): bool
     {
         $result = imap_reopen($imap, $folder);
         if ($result === false) {
-            http_response_code(400);
-            json_encode("Invalid folder!");
+            return false;
         }
         return $result;
     }
-    public function fetchEmails($imap, $criteria): array
+    public function fetchEmails($imap, $criteria): array | null
     {
         $emails = imap_search($imap, $criteria);
         if ($emails === false) {
-            http_response_code(400);
-            json_encode("Invalid search criteria!");
+            return null;
         }
         return $emails;
+    }
+    // prase emails 
+    public function parseEmails($imap, $emails): array
+    {
+        $parsedEmails = [];
+        foreach ($emails as $email) {
+            $parsedEmails[] = imap_fetch_overview($imap, $email);
+        }
+        return $parsedEmails;
+    }
+    // prase body
+    public function parseBody($imap, $email)
+    {
+        $body = imap_fetchbody($imap, $email, 1);
+        return $body;
+    }
+    // parse headers
+    public function parseHeaders($imap, $email)
+    {
+        $headers = imap_fetchheader($imap, $email);
+        if ($headers === false) {
+            return null;
+        }
+        return imap_rfc822_parse_headers($headers);
     }
 }
