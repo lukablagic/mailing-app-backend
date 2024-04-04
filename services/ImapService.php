231e29ebb145999302bf130475900f8014f076d2
Luka Blagic <blagicluka666@gmail.com>
2024-04-04 22:41:15 +0200

    Implement parsing

--- services/ImapService.php
+++ services/ImapService.php
@@ -2,93 +2,51 @@
 
 namespace Service;
 
-use PHPMailer\PHPMailer\PHPMailer;
 use Exception;
+use IMAP\Connection;
 
 class ImapService
 {
-    public function __construct()
+    private  $server = 'imap.gmail.com';
+    private  $port = 993;
+    private  $protocol = 'imap';
+    private  $ssl = 'ssl';
+    private  $folder = 'INBOX';
+
+    public function __construct($server, $port = 993, $folder = 'INBOX', $protocol = 'imap', $ssl = 'ssl',)
     {
-
-
+        $this->server = $server;
+        $this->port = $port;
+        $this->folder = $folder;
+        $this->protocol = $protocol;
+        $this->ssl = $ssl;
     }
 
-    public function sendEmail($email, $password, $data, $attachments)
+    public function connect($email, $password): Connection|false
     {
-        // Instantiate a new PHPMailer object
-        $mail = new PHPMailer(true);
-
-        // Server settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = $email;
-        $mail->Password = $password;
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
-
-        //Recipients
-        try {
-            $mail->setFrom($email);
-            if ($data['to'] != null && is_array($data['to'])) {
-                foreach ($data['to'] as $to) {
-                    if ($to != null) {
-                        $mail->addAddress($to);
-                    }
-                }
-            }
-            if (isset($data['in_reply_to']) && is_array($data['in_reply_to'])) {
-                $mail->addCustomHeader("In-Reply-To", $data['in_reply_to']);
-            }
-            if ($data['cc'] != null && is_array($data['cc'])) {
-                foreach ($data['cc'] as $cc) {
-                    if ($cc != null) {
-                        $mail->addCC($cc);
-                    }
-                }
-            }
-
-            if ($data['bcc'] != null && is_array($data['bcc'])) {
-                foreach ($data['bcc'] as $bcc) {
-                    if ($bcc != null) {
-                        $mail->addBCC($bcc);
-                    }
-                }
-            }
-        } catch (Exception $e) {
+        $imap = imap_open('{imap.gmail.com:993/imap/ssl}INBOX', $email, $password);
+        if ($imap === false) {
             http_response_code(400);
-            json_encode("Invalid recipient parameters!");
+            json_encode("Invalid email or password!");
         }
-
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = $data['subject'];
-        $mail->Body = $data['body'];
-
-        //  echo json_encode($attachments);
-        if ($attachments != null) {
-
-            try {
-                $mail->addAttachment($attachments['tmp_name'], $attachments['name']);
-            } catch (Exception $e) {
-                http_response_code(400);
-                json_encode("Invalid attachment parameters!");
-            }
-
-        }
-        // Send the email
-        try {
-            if (!$mail->send()) {
-                echo 'Message has been sent';
-            }
-
-        } catch (Exception $e) {
-            echo json_encode('Message could not be sent.');
-            echo json_encode('Mailer Error: ' . $mail->ErrorInfo);
-        }
+        return $imap;
     }
-
+    public function openFolder($imap, $folder): bool
+    {
+        $result = imap_reopen($imap, $folder);
+        if ($result === false) {
+            http_response_code(400);
+            json_encode("Invalid folder!");
+        }
+        return $result;
+    }
+    public function fetchEmails($imap, $criteria): array
+    {
+        $emails = imap_search($imap, $criteria);
+        if ($emails === false) {
+            http_response_code(400);
+            json_encode("Invalid search criteria!");
+        }
+        return $emails;
+    }
 }
-
-
-?>
---
+++ services/SmtpService.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Service;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use Exception;
+
+class SmtpService
+{
+    public function __construct()
+    {
+
+
+    }
+
+    public function sendEmail($email, $password, $data, $attachments)
+    {
+        // Instantiate a new PHPMailer object
+        $mail = new PHPMailer(true);
+
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $email;
+        $mail->Password = $password;
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        //Recipients
+        try {
+            $mail->setFrom($email);
+            if ($data['to'] != null && is_array($data['to'])) {
+                foreach ($data['to'] as $to) {
+                    if ($to != null) {
+                        $mail->addAddress($to);
+                    }
+                }
+            }
+            if (isset($data['in_reply_to']) && is_array($data['in_reply_to'])) {
+                $mail->addCustomHeader("In-Reply-To", $data['in_reply_to']);
+            }
+            if ($data['cc'] != null && is_array($data['cc'])) {
+                foreach ($data['cc'] as $cc) {
+                    if ($cc != null) {
+                        $mail->addCC($cc);
+                    }
+                }
+            }
+
+            if ($data['bcc'] != null && is_array($data['bcc'])) {
+                foreach ($data['bcc'] as $bcc) {
+                    if ($bcc != null) {
+                        $mail->addBCC($bcc);
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            http_response_code(400);
+            json_encode("Invalid recipient parameters!");
+        }
+
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $data['subject'];
+        $mail->Body = $data['body'];
+
+        //  echo json_encode($attachments);
+        if ($attachments != null) {
+
+            try {
+                $mail->addAttachment($attachments['tmp_name'], $attachments['name']);
+            } catch (Exception $e) {
+                http_response_code(400);
+                json_encode("Invalid attachment parameters!");
+            }
+
+        }
+        // Send the email
+        try {
+            if (!$mail->send()) {
+                echo 'Message has been sent';
+            }
+
+        } catch (Exception $e) {
+            echo json_encode('Message could not be sent.');
+            echo json_encode('Mailer Error: ' . $mail->ErrorInfo);
+        }
+    }
+
+}
+
+
+?>
