91d4d62e2eac5f393bb2130b83db4931ca0fb818
Luka Blagic <blagicluka666@gmail.com>
2024-04-06 22:47:51 +0200

    Basic imap connection

--- automated/imap-sync.php
+++ automated/imap-sync.php
@@ -2,8 +2,39 @@
 
 namespace Automated;
 
-use PDO;
-use Utility\RequestHandler;
-use Service\AuthService;
-use Model\User;
+require_once '../vendor/autoload.php';
 
+use Config\Database;
+use Model\Teams;
+use Model\TeamsCredentials;
+use Service\ImapService;
+
+$db = new Database();
+$conn = $db->connect();
+
+$teams = new Teams($conn);
+$teamsCredentials = new TeamsCredentials($conn);
+
+$allTeams = $teams->getAll();
+
+
+foreach ($allTeams as $team) {
+    $credentials = $teamsCredentials->getByTeamId($team['id']);
+
+
+    $imapService = new ImapService($credentials['imap_server'], $credentials['imap_port'],  $credentials['protocol'], $credentials['use_ssl'] === 1);
+    $imap = $imapService->connect($credentials['email'], $credentials['password'],  'INBOX');
+    if ($imap === false) {
+        continue;
+    }
+
+    $emails = $imapService->fetchEmails($imap, 'ALL');
+    var_dump($emails);
+    foreach ($emails as $email) {
+        $parsedEmail = $imapService->parseEmails($imap, $email);
+        var_dump($parsedEmail);
+    }
+    if ($imap !== false) {
+        imap_close($imap);
+    }
+}
