8b291a4e92185d6e72cf6b9d0709239ea9f67966
Luka Blagic <blagicluka666@gmail.com>
2024-05-11 14:25:26 +0200

    Display errors from prod

--- automated/sync-all-folders.php
+++ automated/sync-all-folders.php
@@ -3,6 +3,8 @@
 namespace Automated;
 
 ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
 require_once '../vendor/autoload.php';
 
 use Config\Database;
--- automated/sync-status.php
+++ automated/sync-status.php
@@ -3,6 +3,7 @@
 namespace Automated;
 
 ini_set("display_errors", 1);
+error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 
 use Config\Database;
