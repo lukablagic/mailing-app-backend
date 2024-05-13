43607d6a994031093691f56f8dc41e852a6d6da6
Luka Blagic <blagicluka666@gmail.com>
2024-05-13 22:54:06 +0200

    Fix autoloader

--- automated/imap-sync.php
+++ automated/imap-sync.php
@@ -5,7 +5,7 @@ namespace Automated;
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '../vendor/autoload.php';
 
 use Config\Database;
 
